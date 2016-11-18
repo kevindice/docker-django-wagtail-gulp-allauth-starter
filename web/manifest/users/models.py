58377acb46ec53ccc5188dd1e1ccc14be305ed5c
Kevin Dice <kevin.dice1@gmail.com>
2016-11-18 15:29:57 -0600

    Remove gunicorn for now and add model

--- web/manifest/users/models.py
+++ web/manifest/users/models.py
@@ -3,3 +3,8 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+#    balance =
+#    credit_limit =
+#    phone_number =
+#    email = 
+    birthdate = models.DateField()
