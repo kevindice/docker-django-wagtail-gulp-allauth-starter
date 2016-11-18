78371d81c7984f188dbb4e0481dd68db608f5a89
Kevin Dice <kevin.dice1@gmail.com>
2016-11-18 01:18:37 -0600

    Django database settings

--- manifest/manfest/settings.py
+++ manifest/manfest/settings.py
@@ -75,11 +75,13 @@ WSGI_APPLICATION = 'manfest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
