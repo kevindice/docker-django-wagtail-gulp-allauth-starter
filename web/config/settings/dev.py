ccdc5a47def155c5e58ecd662f5f2bff37b131fe
Kevin Dice <kevin.dice1@gmail.com>
2016-11-19 13:28:13 -0600

    Add debug_toolbar and set env to dev for now

--- web/config/settings/dev.py
+++ web/config/settings/dev.py
@@ -1,3 +1,5 @@
 from .base import *
 
 DEBUG=True
+INSTALLED_APPS += ['debug_toolbar']
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
--- web/config/urls.py
+++ web/config/urls.py
@@ -13,10 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
-from django.conf.urls import include
+from django.conf.urls import include, url
+from django.conf import settings
 
-urlpatterns = [
+urlpatterns = []
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
+
+urlpatterns += [
     url(r'^admin/', admin.site.urls),
 ]
