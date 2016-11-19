dafe3fd0250edd459f85ac99956b218884a2cb14
Kevin Dice <kevin.dice1@gmail.com>
2016-11-19 13:03:43 -0600

    Pull secret stuff out of version control

--- web/config/settings/base.py
+++ web/config/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from .secret import Secret
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2o5l&*4jw(@se&3%4cv8o#^vt@c#*uxzte&fnqo3fm=p!(!as)'
+SECRET_KEY = Secret.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
