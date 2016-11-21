78e223c2509aae8ffc717dbbac488de88da27be2
Kevin Dice <kevin.dice1@gmail.com>
2016-11-21 14:37:09 -0600

    Add JumperProfile model

--- web/ksupcapp/profile/admin.py
+++ web/ksupcapp/profile/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from .models import Person
 from .models import Phone
+from .models import JumperProfile
 
 admin.site.register(Person)
 admin.site.register(Phone)
+admin.site.register(JumperProfile)
--- web/ksupcapp/profile/models.py
+++ web/ksupcapp/profile/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import *
 
 HOME='HP'
 CELL='CP'
@@ -13,21 +14,63 @@ PHONE_NUMBER_TYPE_CHOICES = (
 )
 
 class Person(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return self.user.username + '\'s profile'
 
 # Each person has 0 or more phone numbers
 class Phone(models.Model):
-    phone_number = models.CharField(max_length=12)
+    phone_number = models.CharField(
+        max_length=12,
+        help_text="The format <em>xxx-xxx-xxxx</em> is preferred."
+    )
     type = models.CharField(
         max_length=2,
         choices=PHONE_NUMBER_TYPE_CHOICES,
-        default=CELL
+        default=CELL,
     )
     person = models.ForeignKey(Person, on_delete=models.CASCADE)
 
 
     def __str__(self):
         return self.person.user.username + '\'s ' + (self.get_type_display() + ' Phone Number').lower()
+
+class JumperProfile(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    uspa_number = models.PositiveIntegerField(
+        null=True,
+        blank=True,
+        unique=True,
+        validators=[
+            MaxValueValidator(1000000, message="I don't believe USPA numbers exist that are that high."),
+        ]
+    )
+    uspa_expiration_date = models.DateField(
+        null=True,
+        blank=True,
+    )
+    weight = models.PositiveSmallIntegerField(
+        null=True,
+        blank=True,
+        validators=[
+            MinValueValidator(50, message="I'm calling bullshit.  You weigh more than that."),
+            MaxValueValidator(400, message="Let's talk about bowling.")
+        ]
+    )
+    number_of_skydives = models.PositiveSmallIntegerField(
+        default=0,
+    )
+    number_of_skydives_here = models.PositiveSmallIntegerField(
+        default=0,
+    )
+    last_jump_date = models.DateField(
+        null=True,
+        blank=True,
+    )
+
