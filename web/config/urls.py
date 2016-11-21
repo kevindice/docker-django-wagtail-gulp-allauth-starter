7bb8e5151d6535fd41944ccf6135b9e7c8ac7f99
Kevin Dice <kevin.dice1@gmail.com>
2016-11-21 05:34:50 -0600

    Refactor urls.py

--- web/config/urls.py
+++ web/config/urls.py
@@ -32,18 +32,30 @@ urlpatterns = []
 if settings.DEBUG:
     import debug_toolbar
     from django.contrib.staticfiles import views as staticviews
+    from django.conf.urls.static import static
     urlpatterns += [
         url(r'^__debug__/', include(debug_toolbar.urls)),
         url(r'^static/(?P<path>.*)$', staticviews.serve),
-    ]
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # Normal URL Definition
-urlpatterns += [
-    url(r'^accounts/', include('allauth.urls')),
-    url(r'^documents/', include(wagtaildocs_urls)),
+normalpatterns = [
     url(r'^$', core_views.home, name='home'),
-    url(r'^accounts/profile/$', TemplateView.as_view(template_name='profile.html'), name='account_profile'),
     url(r'^admin/', admin.site.urls),
-    url(r'^cms/', include(wagtailadmin_urls)),
-    url(r'', include(wagtail_urls)), # This must be last so that it does not override more specific regexes
 ]
+
+# Auth-Related
+authpatterns = [
+    url(r'^accounts/', include('allauth.urls')),
+    url(r'^accounts/profile/$', TemplateView.as_view(template_name='profile.html'), name='account_profile'),
+]
+
+wagtailpatterns = [
+    url(r'^documents/', include(wagtaildocs_urls)),
+    url(r'^cms/', include(wagtailadmin_urls)),
+    url(r'', include(wagtail_urls)),  # This must be last so that it does not override more specific regexes
+]
+
+urlpatterns += normalpatterns
+urlpatterns += authpatterns
+urlpatterns += wagtailpatterns
