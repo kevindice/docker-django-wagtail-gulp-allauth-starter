307e7fd26cb3e7e7e0ae520b810a2beed7fb5df4
Kevin Dice <kevin.dice1@gmail.com>
2016-11-21 15:19:52 -0600

    Register Rating model with admin

--- web/ksupcapp/profile/admin.py
+++ web/ksupcapp/profile/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from .models import Person
 from .models import Phone
 from .models import JumperProfile
+from .models import Rating
 
 admin.site.register(Person)
 admin.site.register(Phone)
 admin.site.register(JumperProfile)
+admin.site.register(Rating)
