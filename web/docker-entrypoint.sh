f4c3b2a0a0754aff939d23a58930048eabe3360a
Kevin Dice <kevin.dice1@gmail.com>
2016-11-21 05:06:03 -0600

    Remove stale file

--- web/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python manage.py migrate    # Apply database migrations
-
-exec gunicorn config.wsgi:application \
-    --name ksupcapp \
-    --bind 0.0.0.0:8000 \
-    --workers 3 \
-    "$@"
